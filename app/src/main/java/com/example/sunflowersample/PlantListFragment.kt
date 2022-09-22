8e70c8eb86850e81c555b677c5e60e2796d72b5a
mhchoi <mh.choi@altimedia.com>
2022-09-22 20:04:58 +0900

    * PlantListFragment 추가

---
+++ app/src/main/java/com/example/sunflowersample/PlantListFragment.kt
@@ -0,0 +1,19 @@
+package com.example.sunflowersample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sunflowersample.databinding.FragmentPlantListBinding
+
+class PlantListFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding = FragmentPlantListBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/sunflowersample/adapters/SunflowerPagerAdapter.kt
+++ app/src/main/java/com/example/sunflowersample/adapters/SunflowerPagerAdapter.kt
@@ -3,6 +3,7 @@ package com.example.sunflowersample.adapters
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.sunflowersample.GardenFragment
+import com.example.sunflowersample.PlantListFragment
 import java.lang.IndexOutOfBoundsException
 
 const val MY_GARDEN_PAGE_INDEX = 0
@@ -12,7 +13,7 @@ class SunflowerPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment)
 
     private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
         MY_GARDEN_PAGE_INDEX to { GardenFragment() },
-//        PLANT_LIST_PAGE_INDEX to { PlantListFragment() }
+        PLANT_LIST_PAGE_INDEX to { PlantListFragment() }
     )
 
     override fun getItemCount() = tabFragmentsCreators.size
